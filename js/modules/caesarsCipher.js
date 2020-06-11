fa97371958ec6ce9106257a3376b03c302f8ac5e
ak-eventhorizon <45125426+ak-eventhorizon@users.noreply.github.com>
2020-06-11 12:49:57 +0300

    Update caesarsCipher.js

--- js/modules/caesarsCipher.js
+++ js/modules/caesarsCipher.js
@@ -5,8 +5,28 @@
 
 function caesarsCipher(str){
 
-    let encodedString = str;
-    let decodedString = encodedString;
+    let encodedString = str.toUpperCase();
+    let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let shift = 13;
+    let decodedString = '';
+
+    for (let i = 0; i < encodedString.length; i++) {
+
+        let currentLetter = encodedString[i];
+        let currentLetterPosition = alphabet.indexOf(currentLetter);
+        let neededLetterPosition = currentLetterPosition + shift;
+
+        if(currentLetterPosition === -1){
+            decodedString += currentLetter;
+        } else {
+            if(neededLetterPosition < 0) {
+                neededLetterPosition = alphabet.length + neededLetterPosition;
+            } else if(neededLetterPosition > alphabet.length - 1) {
+                neededLetterPosition = neededLetterPosition - alphabet.length;
+            }
+            decodedString += alphabet[neededLetterPosition];
+        }
+    }
 
     return decodedString;
 }
@@ -14,3 +34,4 @@ function caesarsCipher(str){
 
 
 export {caesarsCipher};
+
