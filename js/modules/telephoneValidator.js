696cf54406d715d918beec6c4704294973f1a9d8
ak-eventhorizon <45125426+ak-eventhorizon@users.noreply.github.com>
2020-07-06 13:56:33 +0300

    Preparation for RegExp develop

--- js/modules/telephoneValidator.js
+++ js/modules/telephoneValidator.js
@@ -5,10 +5,30 @@
 
 function telephoneValidator(str){
 
+    let inputStr = str;
+    let telephoneRegex = /^\d+/;
+
+    let validationResult = telephoneRegex.test(inputStr);
+
+    console.log(inputStr.match(telephoneRegex)); // временно для отладки
     
-    return str;
+    return validationResult;
 }
 
+// первым символом может быть одна цифра (а может не быть)
+// затем пробел, тире или скобка
+// затем три цифры
+// затем пробел или тире или закрывающая скобка
+// затем три цифры
+// затем тире или пробел или ничего
+// затем четыре цифры
+
+// 555-555-5555  
+// (555)555-5555  
+// (555) 555-5555  
+// 555 555 5555  
+// 5555555555  
+// 1 555 555 5555  
 
 
 export {telephoneValidator};
