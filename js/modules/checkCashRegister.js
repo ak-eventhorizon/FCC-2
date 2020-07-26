b6eb71e8d028f0cc46546bf1aba220e498ca44e4
ak-eventhorizon <45125426+ak-eventhorizon@users.noreply.github.com>
2020-07-27 00:12:15 +0300

    Update checkCashRegister.js

--- js/modules/checkCashRegister.js
+++ js/modules/checkCashRegister.js
@@ -10,6 +10,18 @@ function checkCashRegister(price, paymentInCash, changeInDrawer){
         change: undefined
     };
 
+    let currency = {
+        penny: 0.01,
+        nickel: 0.05,
+        dime: 0.1,
+        quarter: 0.25,
+        dollar: 1,
+        five: 5,
+        ten: 10,
+        twenty: 20,
+        hundred: 100
+    };
+
     return outputObj;
 }
 
