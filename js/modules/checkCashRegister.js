41388c11da37edb7b54c814adef89ba47b7064a2
ak-eventhorizon <45125426+ak-eventhorizon@users.noreply.github.com>
2020-07-30 11:07:39 +0300

    Update checkCashRegister.js

--- js/modules/checkCashRegister.js
+++ js/modules/checkCashRegister.js
@@ -5,7 +5,6 @@
 // and cash-in-drawer (changeInDrawer) as the third argument
 
 // OUT - object { status: "STATUS", change: [...] }
-//
 
 function checkCashRegister(price, paymentInCash, changeInDrawer){
 
