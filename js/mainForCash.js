98ee2533367bf6a77d2b7f5c6910f3508db793ef
ak-eventhorizon <45125426+ak-eventhorizon@users.noreply.github.com>
2020-08-02 10:24:53 +0300

    Cash Register preps2

--- js/mainForCash.js
+++ js/mainForCash.js
@@ -1,58 +1,10 @@
 'use strict';
 
-import {palindromeChecker} from './modules/palindromeChecker.js';
-import {romanConverter} from './modules/romanConverter.js';
-import {caesarsCipher} from './modules/caesarsCipher.js';
-import {telephoneValidator} from './modules/telephoneValidator.js';
+import {checkCashRegister} from './modules/checkCashRegister.js';
 
-/*************** Palindrome Checker ***************/
+/*************** Check Cash Register ***************/
 
-let inputPalindromeCheck = document.getElementById('palindrome_input');
-let btnPalindromeCheck = document.getElementById('palindrome_button');
-let resultPalindromeCheck = document.getElementById('palindrome_result');
+checkCashRegister();
 
-btnPalindromeCheck.onclick = function(){
-    let result = palindromeChecker(inputPalindromeCheck.value);
-
-    if (result){
-        resultPalindromeCheck.innerHTML = 'PALINDROME!';
-    } else {
-        resultPalindromeCheck.innerHTML = 'not palindrome';
-    }
-};
-
-/*************** Roman Converter ***************/
-
-let inputRomanConverter = document.getElementById('roman_converter_input');
-let resultRomanConverter = document.getElementById('roman_converter_result');
-
-inputRomanConverter.oninput = function(){
-    let convertedNumber = romanConverter(+inputRomanConverter.value);
-    resultRomanConverter.innerHTML = convertedNumber;
-};
-
-/*************** Caesars Cipher ***************/
-
-let inputCipher = document.getElementById('cipher_input');
-let btnCipher = document.getElementById('cipher_button');
-let resultCipher = document.getElementById('cipher_result');
-
-btnCipher.onclick = function(){
-    let decodedString = caesarsCipher(inputCipher.value);
-    resultCipher.innerHTML = decodedString;
-};
-
-/*************** Telephone Number Validator ***************/
-
-let inputTelephone = document.getElementById('telephone_input');
-let resultTelephone = document.getElementById('telephone_result');
-
-inputTelephone.oninput = function(){
-    let result = telephoneValidator(inputTelephone.value);
-
-    if (result){
-        resultTelephone.innerHTML = 'NUMBER IS VALID';
-    } else {
-        resultTelephone.innerHTML = 'NUMBER NOT VALID';
-    }
-};
+// TODO - Сделать отдельную страницу с графическим представлением кассового аппарата:
+// поля с ценой и внесенной суммой, выдвижной лоток с купюрами и монетами
--- js/modules/checkCashRegister.js
+++ js/modules/checkCashRegister.js
@@ -29,8 +29,3 @@ function checkCashRegister(price, paymentInCash, changeInDrawer){
 }
 
 export {checkCashRegister};
-
-/*************** Check Cash Register ***************/
-
-// TODO - Сделать отдельную страницу с графическим представлением кассового аппарата:
-// поля с ценой и внесенной суммой, выдвижной лоток с купюрами и монетами
