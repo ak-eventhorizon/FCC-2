d6752971a8fb288176a3e1e2fb9f22e050b976cd
ak-eventhorizon <45125426+ak-eventhorizon@users.noreply.github.com>
2020-08-06 12:17:20 +0300

    client pocket logic added

--- js/mainForCash.js
+++ js/mainForCash.js
@@ -4,7 +4,59 @@ import {checkCashRegister} from './modules/checkCashRegister.js';
 
 /*************** Check Cash Register ***************/
 
-checkCashRegister();
+let payment = document.querySelector('#payment_field');
+let price = document.querySelector('#price_field');
 
-// TODO - Сделать отдельную страницу с графическим представлением кассового аппарата:
-// поля с ценой и внесенной суммой, выдвижной лоток с купюрами и монетами
+let cashRegister = [
+    [100,0],
+    [20,0],
+    [10,0],
+    [5,0],
+    [1,0],
+    [0.25,0],
+    [0.1,0],
+    [0.05,0],
+    [0.01,0]
+];
+
+let paymentArr = [
+    [100,0],
+    [20,0],
+    [10,0],
+    [5,0],
+    [1,0],
+    [0.25,0],
+    [0.1,0],
+    [0.05,0],
+    [0.01,0]
+];
+
+let changeArr =[
+    [100,0],
+    [20,0],
+    [10,0],
+    [5,0],
+    [1,0],
+    [0.25,0],
+    [0.1,0],
+    [0.05,0],
+    [0.01,0]
+];
+
+payment.value = 0;
+
+let clientPocketBtns = document.querySelectorAll('#app > div.client_pocket > div.cash > div.cell');
+
+// event listeners on Client Pocket buttons
+for (let element of clientPocketBtns) {
+    element.addEventListener('click', function () {
+        let result = parseFloat(payment.value) + parseFloat(element.innerHTML);
+        payment.value = result.toFixed(2);
+    });
+}
+
+
+
+
+
+checkCashRegister();
