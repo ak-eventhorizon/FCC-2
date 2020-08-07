29699760cb76d694636813317afb744e59c739f6
ak-eventhorizon <45125426+ak-eventhorizon@users.noreply.github.com>
2020-08-07 15:17:59 +0300

    Array to Map refactor

--- js/mainForCash.js
+++ js/mainForCash.js
@@ -4,10 +4,25 @@ import {checkCashRegister} from './modules/checkCashRegister.js';
 
 /*************** Check Cash Register ***************/
 
-let payment = document.querySelector('#payment_field');
-let price = document.querySelector('#price_field');
+let paymentField = document.querySelector('#payment_field');
+paymentField.value = 0;
 
-let cashRegister = [
+let priceField = document.querySelector('#price_field');
+
+let cashRegisterSum = 0;
+let cashRegisterContent = new Map([
+    [100,10],
+    [20,10],
+    [10,10],
+    [5,10],
+    [1,10],
+    [0.25,10],
+    [0.1,10],
+    [0.05,10],
+    [0.01,10]
+]);
+
+let paymentFromClient = new Map([
     [100,0],
     [20,0],
     [10,0],
@@ -17,9 +32,9 @@ let cashRegister = [
     [0.1,0],
     [0.05,0],
     [0.01,0]
-];
+]);
 
-let paymentArr = [
+let changeToClient = new Map([
     [100,0],
     [20,0],
     [10,0],
@@ -29,31 +44,32 @@ let paymentArr = [
     [0.1,0],
     [0.05,0],
     [0.01,0]
-];
-
-let changeArr =[
-    [100,0],
-    [20,0],
-    [10,0],
-    [5,0],
-    [1,0],
-    [0.25,0],
-    [0.1,0],
-    [0.05,0],
-    [0.01,0]
-];
-
-payment.value = 0;
+]);
 
 let clientPocketBtns = document.querySelectorAll('#app > div.client_pocket > div.cash > div.cell');
 
 // event listeners on Client Pocket buttons
 for (let element of clientPocketBtns) {
     element.addEventListener('click', function () {
-        let result = parseFloat(payment.value) + parseFloat(element.innerHTML);
-        payment.value = result.toFixed(2);
+        // подсчет и вывод поля Payment
+        let result = parseFloat(paymentField.value) + parseFloat(element.innerHTML);
+        paymentField.value = result.toFixed(2);
+
+        // заполнение Map'a paymentFromClient
+        let currentQuantity = paymentFromClient.get(+element.innerHTML);
+        paymentFromClient.set(+element.innerHTML, ++currentQuantity);
     });
 }
+
+
+
+
+function calculateMapSum(value, key, map){
+    let multiple = key * map.get(key);
+    cashRegisterSum += multiple;
+}
+
+cashRegisterContent.forEach(calculateMapSum);
 
 
 
