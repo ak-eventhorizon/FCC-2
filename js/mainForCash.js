a99cea7f513fcaa6af5d4a6b49762744a688a5d6
ak-eventhorizon <45125426+ak-eventhorizon@users.noreply.github.com>
2020-08-10 14:34:46 +0300

    map summarizer func added

--- js/mainForCash.js
+++ js/mainForCash.js
@@ -11,7 +11,6 @@ let priceField = document.querySelector('#price_field');
 
 let actionButton = document.querySelector('#action_button');
 
-let cashRegisterSum = 0;
 let cashRegisterContent = new Map([
     [100,10],
     [20,10],
@@ -23,6 +22,7 @@ let cashRegisterContent = new Map([
     [0.05,10],
     [0.01,10]
 ]);
+let cashRegisterSum = calculateMapSum(cashRegisterContent);
 
 let paymentFromClient = new Map([
     [100,0],
@@ -35,6 +35,7 @@ let paymentFromClient = new Map([
     [0.05,0],
     [0.01,0]
 ]);
+let paymentSum = calculateMapSum(paymentFromClient);
 
 let changeToClient = new Map([
     [100,0],
@@ -47,6 +48,9 @@ let changeToClient = new Map([
     [0.05,0],
     [0.01,0]
 ]);
+let changeSum = calculateMapSum(changeToClient);
+
+
 
 let clientPocketBtns = document.querySelectorAll('#app > div.client_pocket > div.cash > div.cell');
 
@@ -63,27 +67,39 @@ for (let element of clientPocketBtns) {
     });
 }
 
+// map summarizer
+function calculateMapSum(map){
+    let result = 0;
 
-
-
-function calculateMapSum(value, key, map){
-    let multiple = key * map.get(key);
-    cashRegisterSum += multiple;
+    function calculateMapElement(value, key, map){
+        let multiple = key * map.get(key);
+        result += multiple;
+    }
+    map.forEach(calculateMapElement);
+    return result;
 }
-cashRegisterContent.forEach(calculateMapSum);
+
+
+
 
 
 
 actionButton.addEventListener('click', () => {
     console.log('Payment:');
     console.log(paymentFromClient);
+    console.log('PaymentSum:');
+    console.log(paymentSum);
     console.log('Change:');
     console.log(changeToClient);
+    console.log('ChangeSum:');
+    console.log(changeSum);
     console.log('CashRegister:');
     console.log(cashRegisterContent);
+    console.log('CashRegisterSum:');
+    console.log(cashRegisterSum);
     console.log('Price Field Value:');
     console.log(parseFloat(priceField.value).toFixed(2));
-})
+});
 
 
 
