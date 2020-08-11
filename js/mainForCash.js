4300b48f2a2e4ec2a55b8603ff0926bb82105a26
ak-eventhorizon <45125426+ak-eventhorizon@users.noreply.github.com>
2020-08-11 09:06:29 +0300

    Update mainForCash.js
    Refactor with objects structure

--- js/mainForCash.js
+++ js/mainForCash.js
@@ -4,101 +4,118 @@ import {checkCashRegister} from './modules/checkCashRegister.js';
 
 /*************** Check Cash Register ***************/
 
-let paymentField = document.querySelector('#payment_field');
-paymentField.value = 0;
+const payment = {
+    content: new Map([
+        [100,0],
+        [20,0],
+        [10,0],
+        [5,0],
+        [1,0],
+        [0.25,0],
+        [0.1,0],
+        [0.05,0],
+        [0.01,0]
+    ]),
+    sum: function (){
+        let result = 0;
+    
+        function calculateMapElement(value, key, map){
+            let multiple = key * map.get(key);
+            result += multiple;
+        }
+        this.content.forEach(calculateMapElement);
+        return result;
+    }
+};
 
-let priceField = document.querySelector('#price_field');
+const change = {
+    content: new Map([
+        [100,0],
+        [20,0],
+        [10,0],
+        [5,0],
+        [1,0],
+        [0.25,0],
+        [0.1,0],
+        [0.05,0],
+        [0.01,0]
+    ]),
+    sum: function (){
+        let result = 0;
+    
+        function calculateMapElement(value, key, map){
+            let multiple = key * map.get(key);
+            result += multiple;
+        }
+        this.content.forEach(calculateMapElement);
+        return result;
+    }
+};
 
-let actionButton = document.querySelector('#action_button');
+const register = {
+    content: new Map([
+        [100,10],
+        [20,10],
+        [10,30],
+        [5,30],
+        [1,50],
+        [0.25,80],
+        [0.1,80],
+        [0.05,50],
+        [0.01,50]
+    ]),
+    sum: function (){
+        let result = 0;
+    
+        function calculateMapElement(value, key, map){
+            let multiple = key * map.get(key);
+            result += multiple;
+        }
+        this.content.forEach(calculateMapElement);
+        return result;
+    }
+};
 
-let cashRegisterContent = new Map([
-    [100,10],
-    [20,10],
-    [10,10],
-    [5,10],
-    [1,10],
-    [0.25,10],
-    [0.1,10],
-    [0.05,10],
-    [0.01,10]
-]);
-let cashRegisterSum = calculateMapSum(cashRegisterContent);
+const ui = {
+    payment: document.querySelector('#payment_field'),
+    price: document.querySelector('#price_field'),
+    mainButton: document.querySelector('#action_button'),
+    clientButtons: document.querySelectorAll('div.client_pocket > div.cash > div.cell'),
+};
 
-let paymentFromClient = new Map([
-    [100,0],
-    [20,0],
-    [10,0],
-    [5,0],
-    [1,0],
-    [0.25,0],
-    [0.1,0],
-    [0.05,0],
-    [0.01,0]
-]);
-let paymentSum = calculateMapSum(paymentFromClient);
-
-let changeToClient = new Map([
-    [100,0],
-    [20,0],
-    [10,0],
-    [5,0],
-    [1,0],
-    [0.25,0],
-    [0.1,0],
-    [0.05,0],
-    [0.01,0]
-]);
-let changeSum = calculateMapSum(changeToClient);
-
-
-
-let clientPocketBtns = document.querySelectorAll('#app > div.client_pocket > div.cash > div.cell');
+ui.payment.value = 0;
+ui.price.value = 0;
 
 // event listeners on Client Pocket buttons
-for (let element of clientPocketBtns) {
+for (let element of ui.clientButtons) {
     element.addEventListener('click', function () {
         // подсчет и вывод поля Payment
-        let result = parseFloat(paymentField.value) + parseFloat(element.innerHTML);
-        paymentField.value = result.toFixed(2);
+        let result = parseFloat(ui.payment.value) + parseFloat(element.innerHTML);
+        ui.payment.value = result.toFixed(2);
 
-        // заполнение Map'a paymentFromClient
-        let currentQuantity = paymentFromClient.get(+element.innerHTML);
-        paymentFromClient.set(+element.innerHTML, ++currentQuantity);
+        // заполнение Map'a payment.content
+        let currentQuantity = payment.content.get(+element.innerHTML);
+        payment.content.set(+element.innerHTML, ++currentQuantity);
     });
 }
 
-// map summarizer
-function calculateMapSum(map){
-    let result = 0;
-
-    function calculateMapElement(value, key, map){
-        let multiple = key * map.get(key);
-        result += multiple;
-    }
-    map.forEach(calculateMapElement);
-    return result;
-}
 
 
-
-
-
-
-actionButton.addEventListener('click', () => {
+ui.mainButton.addEventListener('click', () => {
     console.log('Payment:');
-    console.log(paymentFromClient);
+    console.log(payment.content);
     console.log('PaymentSum:');
-    console.log(paymentSum);
+    console.log(payment.sum());
     console.log('Change:');
-    console.log(changeToClient);
+    console.log(change.content);
     console.log('ChangeSum:');
-    console.log(changeSum);
+    console.log(change.sum());
     console.log('CashRegister:');
-    console.log(cashRegisterContent);
+    console.log(register.content);
     console.log('CashRegisterSum:');
-    console.log(cashRegisterSum);
+    console.log(register.sum());
     console.log('Price Field Value:');
-    console.log(parseFloat(priceField.value).toFixed(2));
+    console.log(parseFloat(ui.price.value).toFixed(2));
 });
 
 
